7c782bb7550f8cbfbf129f86a1ada83138c436f2
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2025-05-16 10:34:47 +0200

    First commit

---
+++ src/main/java/org/spring/theguesthouse/TheGuesthouseApplication.java
@@ -0,0 +1,13 @@
+package org.spring.theguesthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGuesthouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheGuesthouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spring/theguesthouse/TheGuesthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.theguesthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGuesthouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
