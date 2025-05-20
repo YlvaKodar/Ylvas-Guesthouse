065b0d93b2134cdf490429455279090fdb8af379
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2025-05-20 16:28:08 +0200

    Customer entity up to date.

--- src/main/java/org/spring/theguesthouse/entity/Customer.java
+++ src/main/java/org/spring/theguesthouse/entity/Customer.java
@@ -1,18 +1,29 @@
 package org.spring.theguesthouse.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Customer {
     @Id
     @GeneratedValue
     private Long id;
+    private String name;
+    private String tel;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Booking> bookings = new ArrayList<>();
+
+    public void addBooking(Booking booking) {
+        bookings.add(booking);
+    }
 }
