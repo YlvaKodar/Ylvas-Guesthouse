daac5ed875cc1fd11afb1563fb43286b167496e2
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2025-05-20 15:41:58 +0200

    Room entity up to date.

--- src/main/java/org/spring/theguesthouse/entity/Room.java
+++ src/main/java/org/spring/theguesthouse/entity/Room.java
@@ -3,14 +3,23 @@ package org.spring.theguesthouse.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Room {
     @Id
     @GeneratedValue
     private Long id;
+    private int roomNumber;
+
+    public Room(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
 }
