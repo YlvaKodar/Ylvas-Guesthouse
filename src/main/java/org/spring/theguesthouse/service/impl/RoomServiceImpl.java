edcbd47eb5df69dad1baf3b079e16186247675cc
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2025-05-20 17:58:04 +0200

    First RoomServiceImpl.

--- src/main/java/org/spring/theguesthouse/service/impl/RoomServiceImpl.java
+++ src/main/java/org/spring/theguesthouse/service/impl/RoomServiceImpl.java
@@ -3,6 +3,7 @@ package org.spring.theguesthouse.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.spring.theguesthouse.dto.RoomDto;
 import org.spring.theguesthouse.entity.Room;
+import org.spring.theguesthouse.repository.RoomRepo;
 import org.spring.theguesthouse.service.RoomService;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -10,18 +11,21 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class RoomServiceImpl implements RoomService {
+
+    private final RoomRepo roomRepo;
+
     @Override
     public RoomDto roomToDto(Room r) {
-        return RoomDto.builder().roomNumber(r.getR).build();
+        return RoomDto.builder().roomNumber(r.getRoomNumber()).build();
     }
 
     @Override
     public Room roomDtoToRoom(RoomDto r) {
-        return null;
+        return Room.builder().roomNumber(r.getRoomNumber()).build();
     }
 
     @Override
     public List<RoomDto> getAllRooms() {
-        return List.of();
+        return roomRepo.findAll().stream().map(this::roomToDto).toList();
     }
 }
