30650463e037da3358bd68ac45f93af9d7de9cdc
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2025-05-20 16:15:00 +0200

    Booking entity up to date.

--- src/main/java/org/spring/theguesthouse/entity/Booking.java
+++ src/main/java/org/spring/theguesthouse/entity/Booking.java
@@ -1,16 +1,36 @@
 package org.spring.theguesthouse.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+import java.util.List;
+
 @Entity
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Booking {
     @Id
     @GeneratedValue
     private Long id;
+
+    private Date startingDate;
+    private Date endingDate;
+
+    @ManyToOne
+    @JoinColumn
+    private Customer customer;
+
+    @ManyToMany
+    @JoinTable()
+    private List<Room> rooms;
+
+    public void addRoom(Room room) {
+        rooms.add(room);
+    }
 }
