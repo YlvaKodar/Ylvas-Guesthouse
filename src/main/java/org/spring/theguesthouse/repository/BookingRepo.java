f81df1c5a2d760ac87a2d63c9fa3bde37d38fb5a
christerbengt <mail@christerbengtsson.se>
2025-05-21 15:35:21 +0200

    Emptied BookingRepo

--- src/main/java/org/spring/theguesthouse/repository/BookingRepo.java
+++ src/main/java/org/spring/theguesthouse/repository/BookingRepo.java
@@ -12,21 +12,5 @@ import java.util.List;
 @Repository
 public interface BookingRepo extends JpaRepository<Booking, Long> {
 
-    // Find all bookings for a specific customer
-    // List<Booking> findByCustomerId(Long customerId);
 
-    // Find all bookings for a specific room
-    // List<Booking> findByRoomId(Long roomId);
-
-    // Check a rooms availability for a given date range
-    // Excludes the booking with the specified ID
-    /* @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END FROM Booking b " +
-            "WHERE b.room.id = :roomId " +
-            "AND b.checkInDate <= :checkOutDate " +
-            "AND b.checkOutDate >= :checkInDate " +
-            "AND (b.id <> :excludeBookingId OR :excludeBookingId IS NULL)")
-    boolean isRoomBooked(@Param("roomId") Long roomId,
-                         @Param("checkInDate") LocalDate checkInDate,
-                         @Param("checkOutDate") LocalDate checkOutDate,
-                         @Param("excludeBookingId") Long excludeBookingId); */
 }
