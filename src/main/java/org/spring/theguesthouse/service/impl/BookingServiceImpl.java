572b1cb182c1318cd720fc328f887991ae7292af
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2025-05-22 12:38:07 +0200

    Removed unnecessary "jag-tänkte-snett"-logic from bookingToDetailedDto.

--- src/main/java/org/spring/theguesthouse/service/impl/BookingServiceImpl.java
+++ src/main/java/org/spring/theguesthouse/service/impl/BookingServiceImpl.java
@@ -1,39 +1,29 @@
 package org.spring.theguesthouse.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.spring.theguesthouse.dto.CustomerDto;
-import org.spring.theguesthouse.dto.RoomDto;
 import org.spring.theguesthouse.entity.Booking;
-import org.spring.theguesthouse.entity.Customer;
-import org.spring.theguesthouse.entity.Room;
 import org.spring.theguesthouse.dto.BookingDTO;
 import org.spring.theguesthouse.dto.DetailedBookingDTO;
-import org.spring.theguesthouse.entity.Booking;
 import org.spring.theguesthouse.repository.BookingRepo;
 import org.spring.theguesthouse.repository.CustomerRepo;
 import org.spring.theguesthouse.repository.RoomRepo;
 import org.spring.theguesthouse.service.BookingService;
+import org.spring.theguesthouse.service.CustomerService;
 import org.spring.theguesthouse.service.RoomService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizers;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BookingServiceImpl implements BookingService {
 
     private final RoomService roomService;
-    // TODO: När CustomerService finns, avkommentera nedan.
-    //  private final CustomerService customerService;
+    private final CustomerService customerService;
 
     private final BookingRepo bookingRepo;
     private final CustomerRepo customerRepo;
     private final RoomRepo roomRepo;
-    private final CacheManagerCustomizers cacheManagerCustomizers;
-
 
     @Override
     public BookingDTO bookingToDto(Booking booking) {
@@ -42,21 +32,11 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public DetailedBookingDTO bookingToDetailedDto(Booking booking) {
-        Customer customer = booking.getCustomer();
-
-        CustomerDto tempCustDto;
-        //TODO: tempCustDto= customerService.customerToDto(customer).
-
-        List<RoomDto> tempRooms = new ArrayList<>();
-        booking.getRooms().forEach(room ->{
-                tempRooms.add(roomService.roomToDto(room));
-        });
-
         return DetailedBookingDTO.builder().id(booking.getId())
                 .start_date(booking.getStart_date()).end_date(booking.getEnd_date())
-                .rooms(tempRooms)
-                //TODO: När customerService har ovanstående metoder, kommentera in nedan.
-                //.customer(tempCustDto)
+                //TODO: När customerService har  metoder, kommentera in nedan.
+                //.customer(new CustomerDto(booking.getCustomer().getId, booking.getCustomer().getName()))
+                .rooms(booking.getRooms().stream().map(roomService::roomToDto).collect(Collectors.toList()))
                 .build();
     }
 
