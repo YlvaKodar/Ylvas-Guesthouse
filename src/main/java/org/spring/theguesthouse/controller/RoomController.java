140ad7874a64b0fc292939fb69372fb5b72f2f97
petra <petrakoski@hotmail.com>
2025-05-25 22:58:28 +0200

    Added a showCreateBooking method and started a createBooking method in BookingController. Also changed the html link in showAllCustomers (addbooking button) to go to /bookings instead oc customers.

--- src/main/java/org/spring/theguesthouse/controller/RoomController.java
+++ src/main/java/org/spring/theguesthouse/controller/RoomController.java
@@ -5,13 +5,30 @@ import org.spring.theguesthouse.dto.RoomDto;
 import org.spring.theguesthouse.entity.Room;
 import org.spring.theguesthouse.repository.RoomRepo;
 import org.spring.theguesthouse.service.RoomService;
+import org.spring.theguesthouse.service.impl.RoomServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+//@RequestMapping((path ="/rooms")
 @RequiredArgsConstructor
 public class RoomController {
+
+    private final RoomRepo roomRepo;
+    private final RoomService roomService;
+
+    @RequestMapping("/available")
+    public String showAvailableRooms(Model model) {
+        List<RoomDto> roomsList = roomService.getAllRooms();
+        model.addAttribute("roomsTitle", "Rooms");
+        model.addAttribute("allRooms", roomsList);
+        model.addAttribute("ID", "id");
+        model.addAttribute("displayRooms", "true");
+        return "showAvailableRooms";
+
+    }
 
 }
