6cf876742e52c26388ca9d185c350f22075eedf0
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2025-05-26 17:14:11 +0200

    CustomerServiceImpltest2: knuten till databas, @BeforeEach-metod, skelett till övriga metoder. (Gjorde denna för att jag är osäker på om funktionerna i den är kompatibla med den andra testfilen och ville inte förstöra i den andra?

---
+++ src/test/java/org/spring/theguesthouse/service/impl/CustomerServiceImplTest2.java
@@ -0,0 +1,108 @@
+package org.spring.theguesthouse.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.spring.theguesthouse.entity.Booking;
+import org.spring.theguesthouse.entity.Customer;
+import org.spring.theguesthouse.entity.Room;
+import org.spring.theguesthouse.repository.BookingRepo;
+import org.spring.theguesthouse.repository.CustomerRepo;
+import org.spring.theguesthouse.repository.RoomRepo;
+import org.spring.theguesthouse.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+@Rollback
+class CustomerServiceImplTest2 {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    private BookingRepo bookingRepo;
+    private RoomRepo roomRepo;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @BeforeEach
+    public void setUp() {
+        customerRepo.deleteAll();
+        bookingRepo.deleteAll();
+        roomRepo.deleteAll();
+
+        Room r1 = Room.builder().roomNumber(101).build();
+        Room r2 = Room.builder().roomNumber(102).build();
+        Room r3 = Room.builder().roomNumber(103).build();
+
+        roomRepo.save(r1);
+        roomRepo.save(r2);
+        roomRepo.save(r3);
+
+        Customer c1 = Customer.builder().name("Maja Gräddnos").tel("018-225162").build();
+        Customer c2 = Customer.builder().name("Gammel-Maja").tel("018-225163").build();
+        Customer c3 = Customer.builder().name("Gullan von Arkadien").tel("018-225164").build();
+
+        customerRepo.save(c1);
+        customerRepo.save(c2);
+        customerRepo.save(c3);
+
+        Booking b1 = Booking.builder().customer(c1)
+                .startDate(LocalDate.of(2025, 10, 15))
+                .endDate(LocalDate.of(2025, 10, 18)).room(r1).build();
+
+        Booking b2 = Booking.builder().customer(c1)
+                .startDate(LocalDate.of(2025, 10, 2))
+                .endDate(LocalDate.of(2025, 10, 3)).room(r2).build();
+
+        Booking b3 = Booking.builder().customer(c3)
+                .startDate(LocalDate.of(2025, 8, 15))
+                .endDate(LocalDate.of(2025, 9, 1)).room(r3).build();
+
+        bookingRepo.save(b1);
+        bookingRepo.save(b2);
+        bookingRepo.save(b3);
+    }
+
+
+    @Test
+    void detailedCustomerDtoToCustomer() {
+    }
+
+    @Test
+    void customerToCustomerDto() {
+    }
+
+    @Test
+    void customerToDetailedCustomerDto() {
+    }
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void getCustomerById() {
+    }
+
+    @Test
+    void addCustomer() {
+    }
+
+    @Test
+    void deleteCustomerById() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+}
